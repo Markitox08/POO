bf89a9ba748fc6d7f8727fb3925549bd24aabd56
Markitox <marcnuki@gmail.com>
2023-02-12 07:55:38 -0600

    marckitox08- 12/02/2023 -creacionLab2

---
+++ LAB2/mFallas_Lab2/Form1.cs
@@ -0,0 +1,10 @@
+namespace mFallas_Lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- VETERINARIA/VETERINARIA/Clases/clsHoja.cs
+++ VETERINARIA/VETERINARIA/Clases/clsHoja.cs
@@ -8,5 +8,47 @@ namespace VETERINARIA.Clases
 {
     internal class clsHoja
     {
+        #region Atributos
+        private int identificador;
+        private string sintomas, diagnostico, tratamiento;
+        private int idMascota;
+        #endregion
+
+        #region Cosntructores
+
+        public clsHoja()
+        {
+            this.identificador = 0;
+            sintomas = "";
+            this.diagnostico = "";
+            this.tratamiento = "";
+            this.idMascota = 0;
+        }
+
+        public clsHoja(int identificador, string sintomas, string diagnostico, string tratamiento, int idMascota)
+        {
+            this.identificador = identificador;
+            this.sintomas = sintomas;
+            this.diagnostico = diagnostico;
+            this.tratamiento = tratamiento;
+            this.idMascota = idMascota;
+        }
+        #endregion
+
+        #region Metodos
+
+        public int identificador
+
+        {
+            set { identificador = value; }
+            get { return identificador; }
+        }
+
+        public string sintomas
+
+        #endregion
+
     }
+
+
 }
